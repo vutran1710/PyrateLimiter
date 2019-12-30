b096dff3aa4ed56daa4bd8b5472f79329a428486
vutr <me@vutr.io>
2019-12-30 09:50:15 +0700

    separate algorithm module

--- pyrate_limiter/core/algorimths.py
+++ pyrate_limiter/core/algorimths.py
@@ -1,16 +1,53 @@
-from .data_structures import (
+""" Providing rate-limitting algorithms
+"""
+from typing import Any
+from enum import Enum
+from .base import (
     AbstractBucket,
     HitRate,
     LoggedItem,
 )
 
 
-class SlidingWindowLog:
-    bucket: AbstractBucket = None
-    rate: HitRate = None
+def sliding_window_log(
+    bucket: AbstractBucket,
+    rate: HitRate,
+    item: Any,
+    now: int,
+):
+    volume = len(bucket)
 
-    def __init__(self, bucket: AbstractBucket, rate: HitRate):
-        """SlidingWindowLog algorithm
-        """
-        self.bucket = bucket
-        self.rate = rate
+    if not volume or volume < rate.hit:
+        logged_item = LoggedItem(item=item, timestamp=now, nth=1)
+        bucket.append(logged_item)
+        return True
+
+    after_leak_volume = volume
+
+    for idx in range(volume):
+        item: LoggedItem = bucket[0]
+        timestamp = item.timestamp
+        if (now - timestamp) > rate.time:
+            after_leak_volume = bucket.discard(number=1)
+        else:
+            break
+
+    if after_leak_volume < rate.hit:
+        logged_item = LoggedItem(
+            item=item,
+            timestamp=now,
+            nth=volume + 1,
+        )
+        bucket.append(logged_item)
+        return True
+
+    return False
+
+
+######################################################
+# Algorithm Enum Class, extensible at much as needed #
+######################################################
+
+
+class Algorithms(Enum):
+    SLIDING_WINDOW_LOG: sliding_window_log
