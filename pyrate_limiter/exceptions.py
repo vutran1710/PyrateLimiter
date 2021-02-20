b3bc6173d29cbb9ae1d63ad274e30143e73cae11
vutr <me@vutr.io>
2021-02-21 00:09:51 +0700

    fix custom exception

--- pyrate_limiter/exceptions.py
+++ pyrate_limiter/exceptions.py
@@ -1,19 +1,29 @@
 # pylint: disable=C0114,C0115
+from json import dumps
+
+
 class BucketFullException(Exception):
-    def __init__(self, identity, rate):
-        super(BucketFullException, self).__init__()
-        self.message = f'Bucket for {identity} with Rate {rate} is already full'
+    def __init__(self, identity, rate, remaining_time):
+        error = f'Bucket for {identity} with Rate {rate} is already full'
+        meta = {
+            "error": error,
+            "identity": identity,
+            "rate": str(rate),
+            "remaining_time": remaining_time,
+        }
+        self.message = dumps(meta)
+        super().__init__(self.message)
 
 
 class InvalidParams(Exception):
     def __init__(self, param_name: str):
-        super(InvalidParams, self).__init__()
         self.message = f'Parameters missing or invalid:{param_name}'
+        super().__init__(self.message)
 
 
 class ImmutableClassProperty(Exception):
     def __init__(self, class_instance, prop: str):
         """ Mutating class property is forbidden
         """
-        super(ImmutableClassProperty, self).__init__()
         self.message = f'{class_instance}.{prop} must not be mutated'
+        super().__init__(self.message)
