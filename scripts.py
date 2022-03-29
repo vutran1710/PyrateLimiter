9b77f68dec5248c222dfa97a58a2bdca31058599
Vu Tran <me@vutr.io>
2022-03-30 00:27:47 +0700

    update cicd

--- scripts.py
+++ scripts.py
@@ -1,18 +1,11 @@
 from subprocess import check_call
 
 
-def format():
-    check_call(["black", "pyrate_limiter/"])
-    check_call(["black", "tests/"])
-    check_call(["isort", "."])
-
-
 def lint() -> None:
     check_call(["pre-commit", "run", "--all-files"])
 
 
 def cover() -> None:
-    check_call(["radon", "mi", "-x", "A", "."])
     check_call(["coverage", "run", "-m", "--source=pyrate_limiter", "pytest", "tests", "--maxfail=1"])
     check_call(["coverage", "report", "-m"])
     check_call(["coverage", "xml"])
@@ -29,4 +22,3 @@ def test() -> None:
             "--fulltrace",
         ]
     )
-    check_call(["radon", "mi", "-x", "A", "."])
