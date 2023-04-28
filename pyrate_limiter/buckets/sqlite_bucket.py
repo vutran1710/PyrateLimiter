42674bf5a2d2eacb09ff936ae7da287cbf2e55ff
vutran1710 <me@vutr.io>
2023-04-28 23:09:29 +0700

    add put to sqlite

--- pyrate_limiter/buckets/sqlite_bucket.py
+++ pyrate_limiter/buckets/sqlite_bucket.py
@@ -1,20 +1,24 @@
 import sqlite3
+from typing import List
 
 from ..abstracts import AbstractBucket
+from ..abstracts import Rate
 from ..abstracts import RateItem
 from ..abstracts import SyncClock
 
 
 class Queries:
     CREATE_BUCKET_TABLE = """
-    CREATE TABLE IF NOT EXISTS '{table}' (name VARCHAR, timestamp INTEGER)
+    CREATE TABLE IF NOT EXISTS '{table}' (
+        name VARCHAR,
+        timestamp INTEGER DEFAULT (strftime('%s','now') || substr(strftime('%f','now'),4))
+    )
     """
     CREATE_INDEX_ON_TIMESTAMP = """
     CREATE INDEX IF NOT EXISTS '{index_name}' ON '{table_name}' (timestamp)
     """
     PUT_ITEMS = """
-    INSERT INTO %s (name, "timestamp")
-    VALUES(%s, datetime('now'))
+    INSERT INTO '{table}' (name) VALUES %s
     """
     REMOVE_ITEMS = """
     -- remove items here
@@ -22,6 +26,11 @@ class Queries:
 
 
 class SQLiteBucket(AbstractBucket):
+    """For sqlite bucket, we are using the sql time function as the clock
+    item's timestamp wont matter here
+    """
+
+    rates: List[Rate]
     conn: sqlite3.Connection
     table: str
 
@@ -31,7 +40,12 @@ class SQLiteBucket(AbstractBucket):
 
     def put(self, item: RateItem) -> bool:
         """Put an item (typically the current time) in the bucket"""
-        pass
+        self.conn.isolation_level = "EXCLUSIVE"
+        self.conn.execute("BEGIN EXCLUSIVE")
+        items = ", ".join(["('%s')" % name for name in [item.name] * item.weight])
+        self.conn.execute(Queries.PUT_ITEMS.format(table=self.table) % items)
+        self.conn.commit()
+        return True
 
     def leak(self, clock: SyncClock) -> int:
         """Schedule a leak and run in a task"""
--- tests/test_sqlite_bucket.py
+++ tests/test_sqlite_bucket.py
@@ -1,14 +1,22 @@
 import sqlite3
 
+from pyrate_limiter.abstracts import RateItem
 from pyrate_limiter.buckets import SQLiteBucket
 from pyrate_limiter.buckets import SQLiteQueries
 
+file_path = "/Users/vutran/pyrate-limiter.sqlite"
+table_name = "pyrate-test-bucket"
+index_name = table_name + "__timestamp_index"
+
+
+def clean_up_db(conn: sqlite3.Connection):
+    conn.execute("DROP TABLE IF EXISTS '%s'" % table_name)
+    conn.execute("DROP INDEX IF EXISTS '%s'" % index_name)
+
 
 def test_bucket_init():
-    file_path = "/Users/vutran/pyrate-limiter.sqlite"
     conn = sqlite3.connect(file_path)
-    table_name = "pyrate-test-bucket"
-    index_name = table_name + "__timestamp_index"
+    clean_up_db(conn)
 
     create_table_query = SQLiteQueries.CREATE_BUCKET_TABLE.format(table=table_name)
     conn.execute(create_table_query)
@@ -24,3 +32,12 @@ def test_bucket_init():
 
     bucket = SQLiteBucket(conn, table_name)
     assert bucket is not None
+
+    bucket.put(RateItem("my-item", 0))
+
+    count = conn.execute("SELECT COUNT(*) FROM '%s'" % table_name).fetchone()[0]
+    assert count == 1
+
+    bucket.put(RateItem("my-item", 0, weight=10))
+    count = conn.execute("SELECT COUNT(*) FROM '%s'" % table_name).fetchone()[0]
+    assert count == 11
