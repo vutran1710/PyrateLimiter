10470989c92072eea999be03b2af197910f6c19d
vutran1710 <me@vutr.io>
2023-04-29 00:30:12 +0700

    update test

--- pyrate_limiter/buckets/sqlite_bucket.py
+++ pyrate_limiter/buckets/sqlite_bucket.py
@@ -1,4 +1,5 @@
 import sqlite3
+from threading import Lock
 from typing import List
 
 from ..abstracts import AbstractBucket
@@ -11,11 +12,15 @@ class Queries:
     CREATE_BUCKET_TABLE = """
     CREATE TABLE IF NOT EXISTS '{table}' (
         name VARCHAR,
-        timestamp INTEGER DEFAULT (strftime('%s','now') || substr(strftime('%f','now'),4))
+        item_timestamp INTEGER DEFAULT (strftime('%s','now') || substr(strftime('%f','now'),4))
     )
     """
     CREATE_INDEX_ON_TIMESTAMP = """
-    CREATE INDEX IF NOT EXISTS '{index_name}' ON '{table_name}' (timestamp)
+    CREATE INDEX IF NOT EXISTS '{index_name}' ON '{table_name}' (item_timestamp)
+    """
+    COUNT_BEFORE_INSERT = """
+    SELECT COUNT(*) FROM '{table}'
+    WHERE item_timestamp >= (strftime('%s','now') || substr(strftime('%f','now'),4)) - {interval}
     """
     PUT_ITEM = """
     INSERT INTO '{table}' (name) VALUES %s
@@ -31,20 +36,37 @@ class SQLiteBucket(AbstractBucket):
     """
 
     rates: List[Rate]
+    lock: Lock
     conn: sqlite3.Connection
     table: str
 
-    def __init__(self, conn: sqlite3.Connection, table: str):
+    def __init__(self, conn: sqlite3.Connection, table: str, rates: List[Rate]):
         self.conn = conn
         self.table = table
+        self.rates = rates
+        self.lock = Lock()
 
     def put(self, item: RateItem) -> bool:
-        self.conn.isolation_level = "EXCLUSIVE"
-        self.conn.execute("BEGIN EXCLUSIVE")
-        items = ", ".join(["('%s')" % name for name in [item.name] * item.weight])
-        self.conn.execute(Queries.PUT_ITEM.format(table=self.table) % items)
-        self.conn.commit()
-        return True
+        with self.lock:
+            self.conn.execute("BEGIN EXCLUSIVE")
+            # Check before insert
+            # NOTE: this part can be rewritten using pure SQL, but its kinda complex,
+            # so I leave it as room for improvement
+            for rate in self.rates:
+                count = self.conn.execute(
+                    Queries.COUNT_BEFORE_INSERT.format(
+                        table=self.table,
+                        interval=rate.interval,
+                    )
+                ).fetchone()[0]
+
+                if count >= rate.limit:
+                    return False
+
+            items = ", ".join(["('%s')" % name for name in [item.name] * item.weight])
+            self.conn.execute(Queries.PUT_ITEM.format(table=self.table) % items)
+            self.conn.commit()
+            return True
 
     def leak(self, clock: SyncClock) -> int:
         """Schedule a leak and run in a task"""
--- tests/test_sqlite_bucket.py
+++ tests/test_sqlite_bucket.py
@@ -1,5 +1,7 @@
 import sqlite3
+from time import sleep
 
+from pyrate_limiter.abstracts import Rate
 from pyrate_limiter.abstracts import RateItem
 from pyrate_limiter.buckets import SQLiteBucket
 from pyrate_limiter.buckets import SQLiteQueries
@@ -9,15 +11,9 @@ table_name = "pyrate-test-bucket"
 index_name = table_name + "__timestamp_index"
 
 
-def clean_up_db(conn: sqlite3.Connection):
+def setup_db(conn: sqlite3.Connection):
     conn.execute("DROP TABLE IF EXISTS '%s'" % table_name)
     conn.execute("DROP INDEX IF EXISTS '%s'" % index_name)
-
-
-def test_bucket_init():
-    conn = sqlite3.connect(file_path)
-    clean_up_db(conn)
-
     create_table_query = SQLiteQueries.CREATE_BUCKET_TABLE.format(table=table_name)
     conn.execute(create_table_query)
 
@@ -30,7 +26,13 @@ def test_bucket_init():
 
     conn.commit()
 
-    bucket = SQLiteBucket(conn, table_name)
+
+def test_bucket_init():
+    conn = sqlite3.connect(file_path, isolation_level="EXCLUSIVE")
+    setup_db(conn)
+
+    rates = [Rate(20, 1000)]
+    bucket = SQLiteBucket(conn, table_name, rates)
     assert bucket is not None
 
     bucket.put(RateItem("my-item", 0))
@@ -41,3 +43,21 @@ def test_bucket_init():
     bucket.put(RateItem("my-item", 0, weight=10))
     count = conn.execute("SELECT COUNT(*) FROM '%s'" % table_name).fetchone()[0]
     assert count == 11
+
+    count = conn.execute(
+        SQLiteQueries.COUNT_BEFORE_INSERT.format(
+            table=table_name,
+            interval=1000,
+        )
+    ).fetchone()[0]
+    assert count == 11
+
+    # Insert 10 more item, it should fail
+    sleep(1)
+    for ntn in range(21):
+        is_ok = bucket.put(RateItem("my-item", 0))
+
+        if ntn == 20:
+            assert is_ok is False
+
+    conn.close()
