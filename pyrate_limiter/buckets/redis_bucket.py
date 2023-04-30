5b047cc2d65f79f437b61846b3649c218374fc1e
vutran1710 <me@vutr.io>
2023-05-01 01:49:03 +0700

    separate method put

--- pyrate_limiter/buckets/redis_bucket.py
+++ pyrate_limiter/buckets/redis_bucket.py
@@ -19,31 +19,35 @@ class LuaScript:
 class RedisSyncBucket(AbstractBucket):
     """A bucket using redis for storing data
     We are not using redis' built-in TIME since it is non-deterministic
-    Can use local server time, but beware of the consistency
-    between server instances
+    In distributed context, use local server time, but beware of
+    the consistency between server instances
     """
 
     rates: List[Rate]
     lock: Lock
     bucket_key: str
 
-    def __init__(self, redis: Redis, bucket_key: str):
+    def __init__(self, rates: List[Rate], redis: Redis, bucket_key: str):
+        self.rates = rates
         self.lock = Lock()
         self.redis = redis
         self.bucket_key = bucket_key
 
+    def _put_item_with_script(self, item: RateItem):
+        self.redis.execute_command(
+            "EVAL",
+            LuaScript.PUT_ITEM,
+            0,  # No key
+            self.bucket_key,
+            item.timestamp,
+            item.name,
+            item.weight,
+        )
+
     def put(self, item: RateItem):
         """Add item to key"""
         with self.lock:
-            self.redis.execute_command(
-                "EVAL",
-                LuaScript.PUT_ITEM,
-                0,  # No key
-                self.bucket_key,
-                item.timestamp,
-                item.name,
-                item.weight,
-            )
+            self._put_item_with_script(item)
 
     def leak(self, clock: Optional[SyncClock] = None) -> int:
         assert clock
--- tests/test_bucket__redis.py
+++ tests/test_bucket__redis.py
@@ -1,16 +1,24 @@
+import pytest
 from redis import Redis
 
+from pyrate_limiter.abstracts import Rate
 from pyrate_limiter.abstracts import RateItem
 from pyrate_limiter.buckets import RedisSyncBucket
 
 BUCKET_KEY = "test-bucket"
 
 
-def test_01():
-    redis = Redis(host="localhost", port=6379, db=0)
-    assert redis.ping()
+@pytest.fixture
+def db():
+    conn = Redis(host="localhost", port=6379, db=0)
+    assert conn.ping()
+    conn.flushall()
+    yield conn
 
-    bucket = RedisSyncBucket(redis, BUCKET_KEY)
+
+def test_01(db, clock):
+    rates = [Rate(5, 1000)]
+    bucket = RedisSyncBucket(rates, db, BUCKET_KEY)
     bucket.flush()
     bucket.put(RateItem("item", 0, weight=10))
-    assert redis.zcard(BUCKET_KEY) == 10
+    assert db.zcard(BUCKET_KEY) == 10
