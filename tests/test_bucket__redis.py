3a565312299d43ba6217d0af0553fad0964ca518
vutran1710 <me@vutr.io>
2023-04-30 17:51:39 +0700

    flush

--- tests/test_bucket__redis.py
+++ tests/test_bucket__redis.py
@@ -9,8 +9,8 @@ BUCKET_KEY = "test-bucket"
 def test_01():
     redis = Redis(host="localhost", port=6379, db=0)
     assert redis.ping()
-    redis.flushall()
 
     bucket = RedisSyncBucket(redis, BUCKET_KEY)
+    bucket.flush()
     bucket.put(RateItem("item", 0, weight=10))
     assert redis.zcard(BUCKET_KEY) == 10
