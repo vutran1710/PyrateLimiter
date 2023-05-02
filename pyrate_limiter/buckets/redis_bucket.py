f484b21da222d016bf67e86317436bfb4a0ba508
vutran1710 <me@vutr.io>
2023-05-02 22:57:13 +0700

    Use single script

--- pyrate_limiter/buckets/redis_bucket.py
+++ pyrate_limiter/buckets/redis_bucket.py
@@ -1,5 +1,5 @@
 import hashlib
-from threading import RLock as Lock
+from threading import Lock
 from typing import List
 from typing import Optional
 
@@ -14,27 +14,28 @@ from ..utils import id_generator
 class LuaScript:
     """Scripts that deal with bucket operations"""
 
-    CHECK_BEFORE_INSERT = """
+    PUT_ITEM = """
     local now = ARGV[1]
     local space_required = tonumber(ARGV[2])
     local bucket = ARGV[3]
+    local item_name = ARGV[4]
+
     for idx, key in ipairs(KEYS) do
-        if idx > 3 then
+        if idx > 4 then
             local interval = tonumber(key)
             local limit = tonumber(ARGV[idx])
             local count = redis.call('ZCOUNT', bucket, now - interval, now)
             local space_available = limit - tonumber(count)
             if space_available < space_required then
-                return idx - 4
+                return idx - 5
             end
         end
     end
-    return -1
-    """
-    PUT_ITEM = """
-    for i=1,ARGV[4] do
-        redis.call('ZADD', ARGV[1], ARGV[2], ARGV[3]..i)
+
+    for i=1,space_required do
+        redis.call('ZADD', bucket, now, item_name..i)
     end
+    return -1
     """
 
 
@@ -57,48 +58,40 @@ class RedisSyncBucket(AbstractBucket):
         self.bucket_key = bucket_key
         self.hasher = hashlib.sha1()
 
-    def _check_before_insert(self, item: RateItem) -> Optional[Rate]:
+    def _check_and_insert(self, item: RateItem) -> Optional[Rate]:
         keys = [
             "timestamp",
             "weight",
             "bucket",
+            "name",
             *[rate.interval for rate in self.rates],
         ]
+
         args = [
             item.timestamp,
             item.weight,
             self.bucket_key,
+            f"{item.name}:{id_generator()}:",  # this is to avoid key collision since we are using ZSET
             *[rate.limit for rate in self.rates],
         ]
-        idx = self.redis.execute_command("EVAL", LuaScript.CHECK_BEFORE_INSERT, len(keys), *keys, *args)
+
+        idx = self.redis.execute_command("EVAL", LuaScript.PUT_ITEM, len(keys), *keys, *args)
 
         if idx < 0:
             return None
 
         return self.rates[idx]
 
-    def _put_item_with_script(self, item: RateItem):
-        self.redis.execute_command(
-            "EVAL",
-            LuaScript.PUT_ITEM,
-            0,  # No key
-            self.bucket_key,
-            item.timestamp,
-            f"{item.name}:{id_generator()}:",  # this is to avoid key collision since we are using ZSET
-            item.weight,
-        )
-
     def put(self, item: RateItem) -> bool:
         """Add item to key"""
         with self.lock:
-            failing_rate = self._check_before_insert(item)
+            failing_rate = self._check_and_insert(item)
 
             if failing_rate:
                 self.failing_rate = failing_rate
                 return False
 
             self.failing_rate = None
-            self._put_item_with_script(item)
             return True
 
     def leak(self, current_timestamp: Optional[int] = None) -> int:
