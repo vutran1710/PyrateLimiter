bab2c0abe7f9517b6e02a25752be7a1da9398e89
vutran1710 <me@vutr.io>
2023-05-04 15:25:03 +0700

    Refactor

--- pyrate_limiter/limiter.py
+++ pyrate_limiter/limiter.py
@@ -1,8 +1,11 @@
 from inspect import iscoroutine
 from inspect import iscoroutinefunction
+from typing import Coroutine
 from typing import Optional
 from typing import Union
 
+from .abstracts import AbstractAsyncBucket
+from .abstracts import AbstractBucket
 from .abstracts import AsyncClock
 from .abstracts import BucketFactory
 from .abstracts import RateItem
@@ -27,7 +30,7 @@ class Limiter:
         bucket_factory.schedule_leak()
         bucket_factory.schedule_flush()
 
-    def wrap_item(self, item_name: str, weight: int) -> RateItem:
+    def wrap_item(self, item_name: str, weight: int) -> Union[RateItem, Coroutine[None, None, RateItem]]:
         if self.clock is None:
             return RateItem(item_name, 0, weight=weight)
 
@@ -41,41 +44,43 @@ class Limiter:
 
         return wrap_async() if iscoroutinefunction(self.clock.now) else wrap_sycn()
 
-    def try_acquire(self, identity: str, weight: int = 1) -> None:
+    def handle_bucket_put(
+        self,
+        bucket: Union[AbstractBucket, AbstractAsyncBucket],
+        item: RateItem,
+    ) -> Union[None, Coroutine[None, None, None]]:
+        def check_acquire(is_success: bool):
+            if not is_success:
+                assert bucket.failing_rate is not None
+                raise BucketFullException(item.name, bucket.failing_rate, -1)
+
+        async def put_async():
+            check_acquire(await bucket.put(item))
+
+        def put_sync():
+            check_acquire(bucket.put(item))
+
+        return put_async() if iscoroutinefunction(bucket.put) else put_sync()
+
+    def try_acquire(self, identity: str, weight: int = 1) -> Union[None, Coroutine[None, None, None]]:
         assert weight >= 0
 
         if weight == 0:
-            return
+            return None
 
         item = self.wrap_item(identity, weight)
 
         if iscoroutine(item):
 
             async def acquire_async():
+                nonlocal item
+                item = await item
                 bucket = self.bucket_factory.get(await item)
-                acquire = bucket.put(item)
-
-                if iscoroutine(acquire):
-                    acquire = await acquire
-
-                if acquire is False:
-                    assert bucket.failing_rate is not None
-                    raise BucketFullException(identity, bucket.failing_rate, -1)
+                result = self.handle_bucket_put(bucket, item)
+                return (await result) if iscoroutine(result) else result
 
             return acquire_async()
 
+        assert type(item) == RateItem
         bucket = self.bucket_factory.get(item)
-
-        if iscoroutinefunction(bucket.put):
-
-            async def acquire_async():
-                acquire = await bucket.put(item)
-                if acquire is False:
-                    assert bucket.failing_rate is not None
-                    raise BucketFullException(identity, bucket.failing_rate, -1)
-
-            return acquire_async()
-
-        if bucket.put(item) is False:
-            assert bucket.failing_rate is not None
-            raise BucketFullException(identity, bucket.failing_rate, -1)
+        return self.handle_bucket_put(bucket, item)
