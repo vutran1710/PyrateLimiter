40b24aace979a202ceee2476ad643614461d142b
vutran1710 <me@vutr.io>
2023-08-13 01:08:44 +0700

    not needed noqa-s

--- pyrate_limiter/buckets/__init__.py
+++ pyrate_limiter/buckets/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 """Conrete bucket implementations
 """
-from .in_memory_bucket import InMemoryBucket  # noqa
-from .redis_bucket import RedisSyncBucket  # noqa
-from .sqlite_bucket import Queries as SQLiteQueries  # noqa
-from .sqlite_bucket import SQLiteBucket  # noqa
+from .in_memory_bucket import InMemoryBucket
+from .redis_bucket import RedisSyncBucket
+from .sqlite_bucket import Queries as SQLiteQueries
+from .sqlite_bucket import SQLiteBucket
