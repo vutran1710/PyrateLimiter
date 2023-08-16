63dcdaf1f9e268f4d9a4ae5aa8198f7cb1b081cd
vutran1710 <me@vutr.io>
2023-08-16 19:26:08 +0700

    clean up

--- pyrate_limiter/exceptions.py
+++ pyrate_limiter/exceptions.py
@@ -1,14 +1,12 @@
 # pylint: disable=C0114,C0115
 from typing import Dict
-from typing import TYPE_CHECKING
 from typing import Union
 
-if TYPE_CHECKING:
-    from .abstracts import Rate
+from .abstracts.rate import Rate
 
 
 class BucketFullException(Exception):
-    def __init__(self, name: str, rate: "Rate"):
+    def __init__(self, name: str, rate: Rate):
         error = f"Bucket for {name} with Rate {rate} is already full"
         self.meta_info: Dict[str, Union[str, float]] = {
             "error": error,
@@ -19,7 +17,7 @@ class BucketFullException(Exception):
 
 
 class LimiterDelayException(Exception):
-    def __init__(self, name: str, rate: "Rate", actual_delay: int, allowed_delay: int):
+    def __init__(self, name: str, rate: Rate, actual_delay: int, allowed_delay: int):
         error = f"""
         Actual delay exceeded allowance: actual={actual_delay}, allowed={allowed_delay}
         Bucket for {name} with Rate {rate} is already full
