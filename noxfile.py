05ea90a35b9f82192284afa97b862b4c8a905a42
vutran1710 <me@vutr.io>
2024-03-16 23:06:23 +0700

    reduce number of processes

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ from nox_poetry import session
 # Reuse virtualenv created by poetry instead of creating new ones
 nox.options.reuse_existing_virtualenvs = True
 
-PYTEST_ARGS = ["--verbose", "--maxfail=1", "--numprocesses=auto"]
+PYTEST_ARGS = ["--verbose", "--maxfail=1", "--numprocesses=4"]
 COVERAGE_ARGS = ["--cov", "--cov-report=term", "--cov-report=xml", "--cov-report=html"]
 
 
