ef36e92a379b24291a2e1a1747ea0276d19ce588
vutran1710 <me@vutr.io>
2024-03-17 15:49:33 +0700

    up

--- tests/test_bucket_factory.py
+++ tests/test_bucket_factory.py
@@ -82,3 +82,4 @@ async def test_factory_leak(clock, create_bucket):
         assert await async_count(factory.buckets[item_name]) == 0
 
     assert len(factory.buckets) == 3
+    factory._leaker.cancel()
