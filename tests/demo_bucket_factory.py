93f77cf809969b8820954b978739d656637971db
huhaoyu.hahahu <huhaoyu.hahahu@bytedance.com>
2024-05-01 00:42:06 +0800

    fix: unit tests
    Change-Id: Id0f0405febbc0d39acacadf35520b9e420b33f7e

--- tests/demo_bucket_factory.py
+++ tests/demo_bucket_factory.py
@@ -11,6 +11,7 @@ from .helpers import flushing_bucket
 from pyrate_limiter import AbstractBucket
 from pyrate_limiter import AbstractClock
 from pyrate_limiter import BucketFactory
+from pyrate_limiter import id_generator
 from pyrate_limiter import InMemoryBucket
 from pyrate_limiter import RateItem
 from pyrate_limiter import RedisBucket
@@ -65,10 +66,11 @@ class DemoBucketFactory(BucketFactory):
 class DemoAsyncGetBucketFactory(BucketFactory):
     """Async multi-bucket factory used for testing schedule-leaks"""
 
-    def __init__(self, bucket_clock: AbstractClock, **buckets: AbstractBucket):
+    def __init__(self, bucket_clock: AbstractClock, auto_leak=False, **buckets: AbstractBucket):
+        self.auto_leak = auto_leak
         self.clock = bucket_clock
         self.buckets = {}
-        self.thread_pool = None
+        self.leak_interval = 300
 
         for item_name_pattern, bucket in buckets.items():
             assert isinstance(bucket, AbstractBucket)
@@ -96,10 +98,16 @@ class DemoAsyncGetBucketFactory(BucketFactory):
 
         pool: AsyncConnectionPool = AsyncConnectionPool.from_url(getenv("REDIS", "redis://localhost:6379"))
         redis_db: AsyncRedis = AsyncRedis(connection_pool=pool)
-        bucket = await RedisBucket.init(DEFAULT_RATES, redis_db, f"test_bucket:{item.name}")
+        key = f"test-bucket/{id_generator()}"
+        await redis_db.delete(key)
+        bucket = await RedisBucket.init(DEFAULT_RATES, redis_db, key)
         self.schedule_leak(bucket, self.clock)
         self.buckets.update({item.name: bucket})
         return bucket
+
+    def schedule_leak(self, *args):
+        if self.auto_leak:
+            super().schedule_leak(*args)
 
     async def flush(self):
         for bucket in self.buckets.values():
