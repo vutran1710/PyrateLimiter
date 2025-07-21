3d59d3c083b351e153a5e9534a82648c3a2a33d0
Paul Timmins <paul@iqmo.com>
2025-07-21 18:48:48 -0400

    combine coverage reports from the MP tests and pytest

--- noxfile.py
+++ noxfile.py
@@ -4,9 +4,10 @@ from nox_poetry import session
 # Reuse virtualenv created by poetry instead of creating new ones
 nox.options.reuse_existing_virtualenvs = True
 
+PYTEST_MP_ARGS = ["--verbose", "--cov=pyrate_limiter", "--cov-append", "--maxfail=1", "tests/test_multiprocessing.py"]
+
 PYTEST_ARGS = ["--verbose", "--maxfail=1", "--numprocesses=auto", "--ignore=tests/test_multiprocessing.py"]
-PYTEST_MP_ARGS = ["--verbose", "--maxfail=1", "tests/test_multiprocessing.py"]
-COVERAGE_ARGS = ["--cov", "--cov-report=term", "--cov-report=xml", "--cov-report=html"]
+COVERAGE_ARGS = ["--cov=pyrate_limiter", "--cov-append", "--cov-report=term", "--cov-report=xml", "--cov-report=html"]
 
 
 @session(python=False)
@@ -17,16 +18,13 @@ def lint(session) -> None:
 @session(python=False)
 def cover(session) -> None:
     """Run tests and generate coverage reports in both terminal output and XML (for Codecov)"""
+    session.run("pytest", *PYTEST_MP_ARGS)
     session.run("pytest", *PYTEST_ARGS, *COVERAGE_ARGS)
 
 
 @session(python=False)
-def test_mp(session) -> None:
-    session.run("pytest", *PYTEST_MP_ARGS)
-
-
-@session(python=False)
 def test(session) -> None:
+    session.run("pytest", *PYTEST_MP_ARGS)
     session.run("pytest", *PYTEST_ARGS)
 
 
