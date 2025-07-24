d15433aa8b3668135e393c3698ace0c5866bedac
paul@iqmo.com <paul@iqmo.com>
2025-07-24 17:51:56 -0400

    raise log verbosity: goal is to reduce noise, but not too much

--- pyrate_limiter/abstracts/bucket.py
+++ pyrate_limiter/abstracts/bucket.py
@@ -190,7 +190,7 @@ class Leaker(Thread):
 
                 await asyncio.sleep(self.leak_interval / 1000)
             except RuntimeError as e:
-                logger.debug("Leak task stopped due to event loop shutdown. %s", e)
+                logger.info("Leak task stopped due to event loop shutdown. %s", e)
                 return
 
     def leak_async(self):
