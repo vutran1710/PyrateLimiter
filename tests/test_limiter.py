50269bfa99fe06068b6593d913b3975fc82137c2
paultiq <104510378+paultiq@users.noreply.github.com>
2025-07-27 01:40:54 -0400

    Make the 50ms buffer configurable (#217)

--- tests/test_limiter.py
+++ tests/test_limiter.py
@@ -101,6 +101,7 @@ async def test_limiter_01(
         factory,
         raise_when_fail=limiter_should_raise,
         max_delay=limiter_delay,
+        buffer_ms=1
     )
     bucket = BucketAsyncWrapper(bucket)
     item = "demo"
@@ -127,7 +128,7 @@ async def test_limiter_01(
                 acquire_ok, cost = await async_acquire(limiter, item)
         else:
             acquire_ok, cost = await async_acquire(limiter, item)
-            assert cost > 400
+            assert cost > 350
             assert acquire_ok
 
     # # Flush before testing again
@@ -183,6 +184,7 @@ async def test_limiter_async_factory_get(
         factory,
         raise_when_fail=limiter_should_raise,
         max_delay=limiter_delay,
+        buffer_ms=5
     )
     item = "demo"
 
@@ -207,7 +209,7 @@ async def test_limiter_async_factory_get(
                 acquire_ok, cost = await async_acquire(limiter, item)
         else:
             acquire_ok, cost = await async_acquire(limiter, item)
-            assert cost > 400
+            assert cost > 350
             assert acquire_ok
 
     # # Flush before testing again
